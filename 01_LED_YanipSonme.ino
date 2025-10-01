8def8f92853644284758c370a2457e2550ca315f
Fatımagül Karakaş <fatiimaguull.krks24@gmail.com>
2025-10-01 22:50:26 +0300

    Create 01_LED_YanipSonme.ino

---
+++ 01_LED_YanipSonme.ino
@@ -0,0 +1,14 @@
+#define led 9
+void setup()
+{
+  pinMode(led, OUTPUT);
+  
+}
+
+void loop()
+{
+  digitalWrite(led, HIGH); // LED yan
+  delay(1000); // 1 saniye bekle
+  digitalWrite(led, LOW); // LED sön
+  delay(1000);  // 1 saniye bekle
+}
